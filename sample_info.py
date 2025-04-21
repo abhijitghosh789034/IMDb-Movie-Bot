fd4ac406f2be4c0f820078b2a394fb9cd907f54f
abhijitghosh789034 <abhijitghosh789034@gmail.com>
2025-04-21 05:37:02 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -32,8 +32,9 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Dingdi")
+DATABASE_URI = environ.get('DATABASE_URI', "
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://abhijitghosh789034:<abhijitghosh789034>@cluster0.jwroecy.mongodb.net/")
+DATABASE_NAME = environ.get('DATABASE_NAME', "mongodb")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
